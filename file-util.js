fb8f2c1a14752f9d18d9692b5ac18257c755682b
Karthikeyan Nallathambi <karthikeyan.nallathambi@gmail.com>
2017-07-11 11:32:44 +0530

    Initial version

---
+++ file-util.js
@@ -0,0 +1,70 @@
+var fs = require('fs');
+
+var FileUtils = function(){}
+var dirTree = [];
+
+var buildFileTree = function (dirName, dirTree, callback) {
+	console.log('Read dir - '+dirName);
+	fs.readdir(dirName, function(err, files){
+		if (err){
+			console.error(err);
+			return callback(err);
+		}
+		var dirPath = fs.realpathSync(dirName);
+		var listLength = files.length;
+		var processCount = 0;
+		if (listLength == 0){
+			callback(null);
+		}
+		files.forEach( function(file){
+			filePath = dirPath + '\\' + file;
+			console.log('Stat - '+filePath);
+			var stat = fs.statSync(filePath);
+			var fileObj = {
+				name: file,
+				path: filePath,
+				type: stat.isDirectory() ? 'dir': 'file',
+				children: []
+			};
+			dirTree.push(fileObj);
+			if ( stat.isDirectory() ){
+				console.log('Process dir - '+filePath);
+				buildFileTree(filePath, fileObj.children, function(err){
+					if (err){
+						return callback(err);
+					} 
+					processCount++;					
+					console.log(listLength+", "+processCount);
+					//callback(null);
+					if (processCount == listLength){
+						console.log('dir callback');
+						callback(null);
+					}
+
+				});
+			} else {
+				console.log('Process file - '+filePath);
+				processCount++;
+				console.log(listLength+", "+processCount);
+				if (processCount == listLength){
+					console.log('file callback');
+					callback(null);
+				}
+			}
+
+		});
+		//callback(null);
+	});
+}
+
+
+
+//module.exports = FileUtils;
+
+buildFileTree('./workspace1', dirTree, function(err){
+	if(err){
+		console.error(err);
+	} else {
+		console.log('Tree - '+JSON.stringify(dirTree, null, 4));
+	}
+});
