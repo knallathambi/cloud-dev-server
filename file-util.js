022ca8049f453070d910bc815f067b6f60f5f4c5
Karthikeyan Nallathambi <karthikeyan.nallathambi@gmail.com>
2017-07-14 11:30:11 +0530

    Release 3

--- file-util.js
+++ file-util.js
@@ -76,18 +76,115 @@ FileUtils.generateHash = function(fileObj){
 	return new Promise(function(resolve, reject){
 		input.on('readable', () => {
 		  const data = input.read();
-		  if (data)
-		    hash.update(data);
-		  else {
+		  var content = "";
+		  if (data) {
+		    hash.update(data);			
+		  } else {
+
 		  	const hashString = hash.digest('hex');
-		    console.log(`${hashString} ${fileObj.name}`);
+		    //console.log(`${hashString} ${fileObj.name}`);
 		    fileObj.hash = hashString;
-		    resolve(hashString);
+
+		    fs.readFile(fileObj.path, 'utf-8', function(err, data){
+		    	if (err){
+		    		reject(err);
+		    	} else {
+		    		fileObj.content = {};
+		    		try {
+		    			fileObj.content = JSON.parse(data);
+		    		} catch(e){
+		    			console.error(e);
+		    			fileObj.content = {
+		    				error: e,
+		    				source: data
+		    			};
+		    		}
+		    		resolve(hashString);
+		    	}
+		    });
+		    
 		  }
 		});	
 	});
 }
 
+FileUtils.createWorkspace = function(workspaceName){
+	return new Promise(function(resolve, reject){
+		fs.mkdir(workspaceName, function(err){
+			if (err){
+				reject(err);
+			} else {
+				resolve();
+			}
+		})
+	});
+}
+
+FileUtils.syncWorkspace = function(workspaceName, dirTree){
+
+	var checkDir = function(dirPath, dirTree, callback){
+		var processCount = 0;
+		dirPath = fs.realpathSync(dirPath);
+		for(var i=0; i< dirTree.length; i++){
+			if (dirTree[i].type == 'dir'){
+				var newDirPath = dirPath + '\\' + dirTree[i].name;
+				if (fs.existsSync(newDirPath)){
+					console.log(`Dir already exists - ${newDirPath}`);
+				} else {
+					fs.mkdirSync(newDirPath);
+					console.log(`Dir created - ${newDirPath}`);
+				}
+
+				checkDir(newDirPath, dirTree[i].children, function(err){
+					if (err){
+						return callback(err);
+					} else {
+						processCount ++;
+						if (processCount == dirTree.length){
+							callback(null);
+						}
+					}
+				});
+			} else {
+				var filePath = dirPath + '\\' + dirTree[i].name;
+				if (fs.existsSync(filePath)){
+					console.log(`File already exists - ${filePath}`);
+					processCount ++;
+					if (processCount == dirTree.length){
+						callback(null);
+					}
+
+				} else {
+					fs.writeFile(filePath, JSON.stringify(dirTree[i].content), function(err){
+						if (err){
+							return callback(err);
+						} else {
+							processCount ++;
+							if (processCount == dirTree.length){
+								callback(null);
+							}
+
+						}
+					})
+				}
+			}
+		}
+	}
+
+	return new Promise(function(resolve, reject){
+		checkDir(workspaceName, dirTree, function(err){
+			if(err){
+				console.error(err);
+				reject(err);
+			} else {
+				//console.log('Tree - '+JSON.stringify(dirTree, null, 4));
+				resolve();				
+			}
+		});
+	});
+
+
+}
 
 module.exports = FileUtils;
 
--- test/file-util-test.js
+++ test/file-util-test.js
@@ -4,14 +4,44 @@ var FileUtils = require('../file-util');
 describe('File Utils', function() {
   describe('Get workspace', function() {
     it('should return workspace as tree', function(done) {
-      FileUtils.getWorkspace('./workspace')
+      FileUtils.getWorkspace('./user_workspace/workspace')
       	.then(function(result){
       		console.log(JSON.stringify(result, null, 2));
       		done();
       	})
       	.catch(function(err){
+      		console.error(err);
       		done();
       	});
     });
   });
+
+  describe('Create workspace', function() {
+    it('should create workspace folder', function(done) {
+      FileUtils.createWorkspace('./user_workspace/karthik')
+      	.then(function(result){
+      		done();
+      	})
+      	.catch(function(err){
+      		console.error(err);
+      		done();
+      	});
+    });
+  });
+
+  describe('Sync workspace', function() {
+    it('should sync workspace folder', function(done) {
+      var dirTree = require('../sample_workspace.json');
+      FileUtils.syncWorkspace('./user_workspace/karthik1', dirTree)
+      	.then(function(result){
+      		console.log('Completed succesfully');
+      		done();
+      	})
+      	.catch(function(err){
+      		console.error(err);
+      		done();
+      	});
+    });
+  });
+
 });
